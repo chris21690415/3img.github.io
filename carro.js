6fb3e8af7efcb6b8d79503c3976dfe28dfac57fa
chris21690415 <21690415@tecvalles.mx>
2024-10-26 12:56:30 -0600

    Add files via upload

---
+++ carro.js
@@ -0,0 +1,8 @@
+function dineros() {
+  document.getElementById("precio").style.fontSize="25px";
+  document.getElementById("precio").innerHTML="1,780,000 MXN <br> Auto de gasolina: Incluye IVA e ISAN. <br> Precio y equipamiento sugerido por BMW de Mexico para la version basica, reservandoce el derecho a modificarlos sin previo aviso.";
+  document.getElementById("precio").style.color="red";
+  document.getElementById("precio").style.backgroundColor="white"
+  document.getElementById("precio").style.border="1px solid black";
+  document.getElementById("precio").style.textAlign="justify";
+}
